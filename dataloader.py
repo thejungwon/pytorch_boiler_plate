ee9dc334b2647a2c0ad56f6f7fdd6d04e7e8ba22
Jungwon Seo <wsbtgt@gmail.com>
2021-02-04 13:30:01 +0900

    Update

--- dataloader.py
+++ dataloader.py
@@ -9,10 +9,13 @@ from torchvision import datasets, transforms
 class CustomDataset(Dataset):
     def __init__(self, data_path):
         pass
-        
 
     def __len__(self):
-        return self.data.shape[0]
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
         
             
 def get_loaders_fromfile(datapath, train_data, valid_data, test_data, batch_size):
